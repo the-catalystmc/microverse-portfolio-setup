7c80d1a58c8e2a150fe1ee59dd7ffc786cfb3868
Rhyine <rhyinestewart@gmail.com>
2021-06-21 17:51:53 -0500

    Remove linter errors.

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const popMenu = document.getElementById('pop-menu');
 const burgerBtn = document.getElementById('burger');
-const listParent = document.getElementById('menu-list')
+const listParent = document.getElementById('menu-list');
 
 let isMenuClose = true;
 
-burgerBtn.addEventListener('click', () => {
-  if(isMenuClose ? openMenu() : closeMenu());
-  isMenuClose = !isMenuClose;
-});
-
-listParent.addEventListener("click", function(e) {
-  if(e.target && e.target.nodeName == "A") {
-      closeMenu();
-  }
-});
-
 function openMenu() {
   popMenu.style.display = 'flex';
-};
+}
 
 function closeMenu() {
   popMenu.style.display = 'none';
-};
+}
+
+burgerBtn.addEventListener('click', () => {
+  if (isMenuClose ? openMenu() : closeMenu());
+  isMenuClose = !isMenuClose;
+});
+
+listParent.addEventListener('click', (e) => {
+  if (e.target && e.target.nodeName === 'A') {
+    closeMenu();
+  }
+});
