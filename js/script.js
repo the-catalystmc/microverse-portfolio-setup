a55217b93a22532c7c63397110a274890faa71ea
Gabriel <gabriel.gabrielyea@gmail.com>
2021-06-23 13:11:51 -0500

    Add data to modal window

--- js/script.js
+++ js/script.js
@@ -1,9 +1,71 @@
-
 const popMenu = document.getElementById('pop-menu');
 const burgerBtn = document.getElementById('burger');
 const listParent = document.getElementById('menu-list');
+const projects = document.getElementsByClassName('project-1');
+const popWindow = document.getElementsByClassName('popup-window');
+
+//POPUP WINDOW SETUP
+let body = document.querySelector('body');
+let openPopup = document.querySelector('.popup-btn');
+let closePopup = document.querySelector('.close-btn');
+let popupWindow = document.querySelector('.popup-window');
+let popupBg = document.querySelector('.popup-bg');
 
 let isMenuClose = true;
+
+//Projects Data
+const myData = [{
+    projectTitle: "Project 1",
+    projectDesc: 'Laboris sint quis velit id in Lorem nulla labore amet eu do.'
+  },
+  {
+    projectTitle: "Project 2",
+    projectDesc: 'Laboris sint quis balablbalbalaa labore amet eu do.'
+  },
+  {
+    projectTitle: "Project 3",
+    projectDesc: 'Laboris sint quis velit idbalbalbalbalblaba.'
+  },
+  {
+    projectTitle: "Project 4",
+    projectDesc: 'blablablablaba id in Lorem nulla labore amet eu do.'
+  },
+  {
+    projectTitle: "Project 4",
+    projectDesc: 'blablablablaba id in Lorem nulla labore amet eu do.'
+  },
+];
+
+// ----- FUNCTIONS DECLARATIONS------
+function openProjectModal() {
+  popupBg.style.display = 'flex';
+  popupWindow.style.display = 'block';
+}
+
+function closeProjectModal() {
+  popupWindow.style.display = 'none';
+  popupBg.style.display = 'none';
+}
+
+const setUpModalData = (index) => {
+  popWindow[0].querySelector('.project-name').innerText = myData[index].projectTitle;
+  popWindow[0].querySelector('#mobile-pop-text').textContent = myData[index].projectDesc;
+  popWindow[0].querySelector('#desk-pop-text').textContent = myData[index].projectDesc;
+  console.log(popWindow[0].querySelector(('#pop-text')))
+};
+
+const setUpProjectsData = () => {
+  Object.entries(projects).forEach((arr) => {
+    let index = arr[0];
+    let currentProject = arr[1];
+    currentProject.querySelector('.project-title').innerText = myData[index].projectTitle;
+    currentProject.querySelector('.primary-read').innerText = myData[index].projectDesc;
+    currentProject.querySelector('.popup-btn').addEventListener('click', (source) => {
+      openProjectModal();
+      setUpModalData(index);
+    });
+  });
+};
 
 function openMenu() {
   popMenu.style.display = 'flex';
@@ -12,6 +74,10 @@ function openMenu() {
 function closeMenu() {
   popMenu.style.display = 'none';
 }
+//------------------------------------
+
+// Function calls
+setUpProjectsData();
 
 burgerBtn.addEventListener('click', () => {
   if (isMenuClose ? openMenu() : closeMenu());
@@ -24,116 +90,20 @@ listParent.addEventListener('click', (e) => {
   }
 });
 
-//POPUP WINDOW SETUP
 
-let body = document.querySelector('body');
-let openPopup = document.querySelector('.popup-btn');
-let closePopup = document.querySelector('.close-btn');
-let popupWindow = document.querySelector('.popup-window');
-let popupBg = document.querySelector('.popup-bg');
 
-function openWindow() {
-  popupBg.style.display = 'flex';
-  popupWindow.style.display = 'block';
- 
-}
-
-function closeWindow() {
-  popupWindow.style.display = 'none';
-  popupBg.style.display = 'none';
-
-}
-
-openPopup.addEventListener('click', () => {
-    console.log('Window Open')
-    openWindow();
-})
+// ----EVENTS-----
+// openPopup.addEventListener('click', (source) => {
+//   console.log(source.target);
+//   openProjectModal();
+// })
 
 closePopup.addEventListener('click', () => {
-  closeWindow();
+  closeProjectModal();
   console.log('Window Close')
 
 })
 
-//Projects Class
-class Project {
-  projectIndex;
+window.addEventListener('load', () => {
 
-  name;
-
-  description;
-
-  projectImage;
-
-  technologies = [];
-
-  liveLink;
-
-  sourceLink;
-
-  constructor(
-    projectIndex,
-    name,
-    description,
-    projectImage,
-    technologies,
-    liveLink,
-    sourceLink,
-  ) {
-    this.projectIndex = projectIndex;
-    this.name = name;
-    this.description = description;
-    this.projectImage = projectImage;
-    this.technologies = technologies;
-    this.liveLink = liveLink;
-    this.sourceLink = sourceLink;
-  }
-}
-
-export const projectsData = [
-  new Project('0',
-    'Tonic',
-    '  A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    '../images/project1.svg',
-    [
-      'html',
-      'css',
-      'javascript',
-    ],
-    'link1',
-    'link2'),
-  new Project('1',
-    'Multi-Purpose Stories',
-    '  A daily selection of privately personalized reads; no accounts or sign-ups required.',      
-    '../images/project2.svg',
-    [
-      'ruby',
-      'javascript',
-    ],
-    'link1',
-    'link2'),
-  new Project('2',
-    'Tonic',
-    '  A daily selection of privately personalized reads; no accounts or sign-ups required.',      
-    '../images/project3.png',
-    [
-      'html',
-      'css',
-      'javascript',
-    ],
-    'link1',
-    'link2'),
-  new Project('3',
-    'Multi-Purpose Stories',
-    '  A daily selection of privately personalized reads; no accounts or sign-ups required.',      
-    '../images/project4.svg',
-    [
-      'html',
-      'css',
-      'javascript',
-    ],
-    'link1',
-    'link2')
-];
-
-
+})
